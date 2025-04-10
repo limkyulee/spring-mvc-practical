e4efe91ae371f1ec8873dcff00454a7434c6cf1b
limkyulee <mud0308@gmail.com>
2025-04-10 11:22:25 +0900

    Initial Commit

---
+++ src/main/java/hello/spring_mvc_practical/SpringMvcPracticalApplication.java
@@ -0,0 +1,13 @@
+package hello.spring_mvc_practical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcPracticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcPracticalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/spring_mvc_practical/SpringMvcPracticalApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.spring_mvc_practical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcPracticalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
